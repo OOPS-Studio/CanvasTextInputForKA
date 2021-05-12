076d09975da2c3c1b4b277d97b4ad87d22ab3955
OOPS! Studio! <46879790+OOPS-Studio@users.noreply.github.com>
2021-05-12 16:04:29 -0700

    Update forwebpage.js

--- forwebpage.js
+++ forwebpage.js
@@ -637,8 +637,8 @@ if(typeof TextInput === "undefined"){
                     var keys2 = Object.keys(obj.onSelect);
                     for(var j = 0;j < keys2.length;j++){
                         if(keys2[j] === "padding"){
-                            let cross = ["Top","Right","Bottom","Left"];
-                            for(let k = 0;k < obj.onSelect.padding.length;k++){
+                            var cross = ["Top","Right","Bottom","Left"];
+                            for(var k = 0;k < obj.onSelect.padding.length;k++){
                                 if(obj.padding[k] !== false){
                                     this.style.onSelect["padding" + cross[k]] = obj.onSelect.padding[k];
                                 }
@@ -647,8 +647,8 @@ if(typeof TextInput === "undefined"){
                         this.style.onSelect[keys2[j]] = obj.onSelect[keys2[j]];
                     }
                 }else if(keys[i] === "padding"){
-                    let cross = ["Top","Right","Bottom","Left"];
-                    for(let j = 0;j < obj.padding.length;j++){
+                    var cross = ["Top","Right","Bottom","Left"];
+                    for(var j = 0;j < obj.padding.length;j++){
                         if(obj.padding[j] !== false){
                             this.style["padding" + cross[j]] = obj.padding[j];
                         }
